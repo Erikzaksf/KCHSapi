8125bbf8a97275f6bca4a7ac150bf0956e8dedd7
Guest User <Guest@epicodus-27.local>
2017-12-20 09:03:57 -0800

    fix table not found error

--- db/migrate/20171215174605_add_animals.rb
+++ db/migrate/20171215174605_add_animals.rb
@@ -1,12 +1,14 @@
 class AddAnimals < ActiveRecord::Migration[5.1]
   def change
-    create_table :quotes do |t|
+    create_table :animals do |t|
       t.column :name, :string
       t.column :species, :string
       t.column :breed, :string
       t.column :age, :integer
       t.column :about, :string
       t.column :gender, :string
+
+      
     end
   end
 end
--- db/migrate/20171215180828_change_table_name_retry.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTableNameRetry < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :quotes, :animals
-  end
-end
